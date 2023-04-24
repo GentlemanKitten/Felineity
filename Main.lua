bb8af3dc84593d429176944c40c21ea3fe07de84
GentlemanKitten <pijus.skirmantas@gmail.com>
2023-04-24 16:47:00 +0300

    -

---
+++ Main.lua
@@ -0,0 +1,20 @@
+local URL = "https://raw.githubusercontent.com/GentlemanKitten/Felineity/main/Games"
+
+local games = {
+    
+}
+
+for i,v in next, games do
+    games[i] = table.concat(v:split(' '), '_')
+end
+
+local name = games[game.PlaceId] or games[game.GameId]
+
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
+return loadstring(game:HttpGet(URL .. "/" .. (name or "Universal")..".lua", true))()
